91b96982b2c762359b446d59198d14b770c78239
Miško Dvořák <michal.dvorak999@gmail.com>
2022-06-12 11:10:26 +0200

    Making knexfile db donnection provate

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,17 @@
 export default{
     client: 'mysql2',
     connection: {
+        host: "sql11.freemysqlhosting.net",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "sql11499219"
+    },
+    useNullAsDefault: false,
+}
+/*export default{
+    client: 'mysql2',
+    connection: {
         host: "127.0.0.1",
         port: 3306,
         user: "root",
@@ -8,4 +19,4 @@ export default{
         database: "socket_draw"
     },
     useNullAsDefault: false,
-}
+}*/
